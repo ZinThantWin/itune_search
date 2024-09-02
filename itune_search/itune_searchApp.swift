82168a5fde6c6feeadd703ebdab4fd9f662dfa0e
ZT <zinthantwin1997@gmail.com>
2024-09-02 11:14:16 +0630

    Initial Commit

---
+++ itune_search/itune_searchApp.swift
@@ -0,0 +1,17 @@
+//
+//  itune_searchApp.swift
+//  itune_search
+//
+//  Created by ကင်ဇို on 02/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct itune_searchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
